abf38e5be70bcd02d45ea04e8f01c702f0ed3bca
gustavohdev <g_a93@outlook.com>
2021-09-01 13:31:50 -0300

    axios, form, css, readme - all added

---
+++ README.md
@@ -0,0 +1,1 @@
+# Requiriments
--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!")
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Hello, World!")
+
+    const form = document.querySelector("form")
+    console.log(form)
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const username = document.querySelector("input").value
+        alert(username)
+        //alert("Pressed button!")
+    })
+
+})
